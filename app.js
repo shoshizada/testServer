652d73d3922ea2699c2281c5db2832eef85bf1c4
shoshizada <107625484+shoshizada@users.noreply.github.com>
2023-11-05 15:40:14 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+//פונקציה לקידום תשעה תמונות
+app.get("/api/photos", (req, res) => {
+  console.log("YEY");
+  const itemsPerPage = 9;
+  const currentPage = parseInt(req.query.currentPage, 10) || 1;
+  const photos = (req.query.photos || "[]");
+
+
+  const startIndex = currentPage +1;
+  const endIndex = startIndex + itemsPerPage;
+  const paginatedPhotos = photos.slice(startIndex, endIndex);
+  console.log(paginatedPhotos);
+
+  try {
+    res.send({ hits: paginatedPhotos }); // אם אין שגיאות, שלח תשובה
+  } catch (error) {
+    console.error("Error:", error); // הדפס שגיאה ללוגים אם קיימת
+    res.status(500).send("Internal Server Error"); // שלח קוד שגיאה 500 בתשובה
+  }
+});
+
+module.exports = app;
